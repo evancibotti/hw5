a613fdabc8733bc225913c42ebe7d0c6e51d9096
evancibotti <cibotti@usc.edu>
2025-04-26 02:08:39 +0000

    Finished implementations

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,7 +21,15 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+bool helperfunct(
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    vector<size_t>& shiftsworked,
+    size_t day,
+    size_t numworker
+);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -37,9 +45,80 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
+    // save the number of workers and days
+    size_t workers = avail[0].size();
+    size_t days = avail.size();
 
+    // initialize schedule
+    sched = DailySchedule(days, vector<Worker_T>());
 
+    // initialize shiftsworked to 0
+    vector<size_t>shiftsworked(workers, 0);
 
+    // return the helper functions boolean
+    return helperfunct(avail, dailyNeed, maxShifts, sched, shiftsworked, 0, 0);
+}
 
+bool helperfunct(
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    vector<size_t>& shiftsworked,
+    size_t day,
+    size_t numworker
+)
+{
+    // base case for if all the days are scheduled
+    if(day == avail.size()){
+        return true;
+    }
+
+    // recursive call for next day if the day is full
+    if(dailyNeed == numworker){
+        return helperfunct(avail, dailyNeed, maxShifts, sched, shiftsworked, day+1, 0);
+    }
+
+    // loop through all of the workers
+    for(size_t w = 0; w < avail[0].size(); ++w){
+        // check if the worker is still available for the day
+        if(avail[day][w] == true){
+            bool scheduled = false;
+        
+            // now workers assigned for the day in schedule
+            for(size_t a = 0; a < sched[day].size(); ++a){
+                // see if the worker is on schedule
+                if(sched[day][a] == w){
+                    // update this if so and break
+                    scheduled = true;
+                    break;
+                }
+            }
+
+            // now check further things if the worker still isnt assigned
+            if(scheduled == false){
+                // make sure the worker hasn't worked all of his shifts
+                if(maxShifts > shiftsworked[w]){
+                    // assign the worker to today
+                    sched[day].push_back(w);
+                    // increment shiftsworked
+                    shiftsworked[w]++;
+                
+
+                    // recursive call
+                    if(helperfunct(avail, dailyNeed, maxShifts, sched, shiftsworked, day, numworker+1)){
+                        return true;
+                    }
+
+                    // backtrack by decrementing shifts worked
+                    shiftsworked[w]--;
+                    // backtrack by removing the worker from schedule for the day
+                    sched[day].pop_back(); 
+                } 
+            }
+        }   
+    }
+    // no worker works
+    return false;
 }
 
--- wordle.cpp
+++ wordle.cpp
@@ -13,16 +13,78 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+void helperfunct(string& currword, int currindex, string floating, const set<string>& valwords, set<string>& answers);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
     const std::string& in,
     const std::string& floating,
-    const std::set<std::string>& dict)
-{
-    // Add your code here
-  
+    const std::set<std::string>& dict){
+  // Add your code here
+  // set initial
+  string currword = in;
+  // create set of strings to return
+  set<string> answers;
+  // call helper
+  helperfunct(currword, 0, floating, dict, answers);
+  // return
+  return answers;
 }
 
 // Define any helper functions here
+void helperfunct(string& currword, int currindex, string floating, const set<string>& valwords, set<string>& answers){
+  // base case for helper is when the word is complete
+  if(static_cast<size_t>(currindex) == currword.size()){
+    if(floating.empty() == true){
+      if(valwords.find(currword) != valwords.end()){
+        answers.insert(currword);
+      }
+    }
+    // end the function
+    return;
+  }
+
+  // make sure theres not a letter in the current spot in the word
+  if(currword[currindex] != '-'){
+    // call helper and progress into the word
+    helperfunct(currword, currindex+1, floating, valwords, answers);
+  }
+
+  // now try to fill in letters if its not
+  if(currword[currindex] == '-'){
+    // check the floating letters to see if they fit
+    for(size_t i = 0; i < floating.size(); ++i){
+      // insert into word
+      currword[currindex] = floating[i];
+      // save new floating string without character
+      string tempfloat = floating;
+      tempfloat.erase(i, 1);
+      // recursive call
+      helperfunct(currword, currindex+1, tempfloat, valwords, answers);
+      // backtrack 
+      currword[currindex] = '-';
+    }
+
+    // now check if a non floating letter can be used
+    // figure out how many blanks are left
+    int blanksamt = 0;
+    for(size_t i = currindex+1; i < currword.size(); ++i){
+      if(currword[i] == '-'){
+        blanksamt++;
+      }
+    }
+
+    // only do it if theres enough blanks 
+    if(floating.size() <= static_cast<size_t>(blanksamt)){
+      // try all letters
+      for(char c = 'a'; c <= 'z'; ++c){
+        // insert into word
+        currword[currindex] = c;
+        // call recursively
+        helperfunct(currword, currindex+1, floating, valwords, answers);
+        // backtrack 
+        currword[currindex] = '-';
+      }
+    }
+  }
+}
